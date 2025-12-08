299a90ea5a372850a450adef66c3da0ae8f096a1
Mohit Rao Jadhav <anshurao725@gmail.com>
2025-12-08 19:08:30 +0530

    Create 1925. Count Square Sum Triples.cpp

---
+++ 1925. Count Square Sum Triples.cpp
@@ -0,0 +1,19 @@
+// C++ Solution
+
+class Solution {
+ public:
+  int countTriples(int n) {
+    int ans = 0;
+    unordered_set<int> squared;
+
+    for (int i = 1; i <= n; ++i)
+      squared.insert(i * i);
+
+    for (const int a : squared)
+      for (const int b : squared)
+        if (squared.contains(a + b))
+          ++ans;
+
+    return ans;
+  }
+};
